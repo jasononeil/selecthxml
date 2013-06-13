beab4686cc3ca7a9df172ecc4a483f3a01287333
Jason O'Neil <jason.oneil@gmail.com>
2013-06-13 09:53:39 +0800

    Changes to release.sh to package differently

--- release.sh
+++ release.sh
@@ -2,10 +2,10 @@
 
 mkdir -p bin
 
-cp -R src/* bin/
+cp -R haxelib.json README.md src bin/
 
 cd bin
-zip -r selecthxml.zip haxelib.json selecthxml
+zip -r selecthxml.zip haxelib.json README.md src
 cd ..
 
 echo "Packaged in bin/selecthxml.zip"
